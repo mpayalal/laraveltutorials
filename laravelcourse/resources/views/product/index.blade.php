9960a25778b673eed1729320da53f647c1a29904
Mapa <mpayalal@eafit.edu.co>
2023-08-23 10:19:38 -0500

    Added cart section

--- laravelcourse/resources/views/product/index.blade.php
+++ laravelcourse/resources/views/product/index.blade.php
@@ -9,7 +9,7 @@
                 <img src="https://laravel.com/img/logotype.min.svg" class="card-img-top img-card">
                 <div class="card-body text-center">
                     <a href="{{ route('product.show', ['id'=> $product["id"]]) }}" 
-                    class="btn bg-primary text-white">{{ $product["name"] }}</a>
+                     >{{ $product["name"] }}</a>
                 </div>
             </div>
         </div>
